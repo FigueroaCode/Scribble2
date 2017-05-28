325ac7270d284c56e5d45e0c8872d9761b995a37
debrian <debrianfigueroa@gmail.com>
2017-05-28 19:48:56 -0400

    added firebase dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { SignInPage } from '../pages/sign_in/sign_in';
 import { RegisterPage} from '../pages/register/register';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBigCugTwNIJnJ2HKVV4GHGzjRxBYYc0Lk",
+    authDomain: "scribbledb-eb51b.firebaseapp.com",
+    databaseURL: "https://scribbledb-eb51b.firebaseio.com",
+    projectId: "scribbledb-eb51b",
+    storageBucket: "scribbledb-eb51b.appspot.com",
+    messagingSenderId: "317744421633"
+}
 
 @NgModule({
   declarations: [
@@ -18,7 +28,8 @@ import { RegisterPage} from '../pages/register/register';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
