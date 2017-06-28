82db018f157dd5020fa1bff6855bd84bf96c178e
debrian <debrianfigueroa@gmail.com>
2017-06-28 17:07:03 -0400

    small change

--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -14,6 +14,30 @@ export class AuthService {
         return this.fireAuth;
     }
 
+    googleLogIn(){
+        let provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+        firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+        }).catch(function(error) {
+          // Handle Errors here.
+         // var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          //var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          //var credential = error.credential;
+          // ...
+        });
+    }
+
+
     doLogin(email: string, password: string): any {
     return this.fireAuth.signInWithEmailAndPassword(email, password);
     }
