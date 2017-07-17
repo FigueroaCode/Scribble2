9d8a6d2dfb5a57c3772f8eb9d8ee88836ec9c98f
Luis Garcia <garcialuis1998@yahoo.es>
2017-07-17 17:39:47 -0400

    Working on slides.

--- src/pages/joincourse/joincourse.ts
+++ src/pages/joincourse/joincourse.ts
@@ -10,14 +10,24 @@ import { AuthService } from '../../providers/auth-service';
 })
 
 export class JoinCoursePage {
-
     courses: FirebaseListObservable<any[]>;
-    name: string;
+    coursesToShow: FirebaseListObservable<any[]>;
+    appliedFilters: {
+      title: string,
+      courseID: string,
+      professor: string,
+      university: string
+    };
 
     constructor(public navCtrl: NavController, public firebaseService: FirebaseService,
         public authService: AuthService, public alertCtrl: AlertController) {
         this.initializeList();
-        this.name = "Lalalaa";
+        this.appliedFilters = {
+          title: '',
+          courseID: '',
+          professor: '',
+          university: ''
+        };
     }
 
     initializeList(){
@@ -26,39 +36,40 @@ export class JoinCoursePage {
 
     //Filter Items for course list.
     filterItems(target: string, type: string) {
+      
       // if the value is an empty string don't filter the items
       if (target && target.trim() != '' && type == 'courseNameInput') {
-          this.courses = this.firebaseService.getDB().list('/courses', {
-              query:
-              {
-                  orderByChild: 'title',
-                  equalTo: target
-              }
-          });
+          this.appliedFilters.title = target;
       }else if (target && target.trim() != '' && type == 'courseID') {
-          this.courses = this.firebaseService.getDB().list('/courses', {
-              query:
-              {
-                  orderByChild: 'courseID',
-                  equalTo: target
-              }
-          });
+          this.appliedFilters.courseID = target;
       }else if (target && target.trim() != '' && type == 'professor') {
-          this.courses = this.firebaseService.getDB().list('/courses', {
-              query:
-              {
-                  orderByChild: 'professor',
-                  equalTo: target
-              }
-          });
+          this.appliedFilters.professor = target;
       } else if (target && target.trim() != '' && type == 'university') {
-          this.courses = this.firebaseService.getDB().list('/courses', {
-              query:
-              {
-                  orderByChild: 'university',
-                  equalTo: target
-              }
-          });
+          this.appliedFilters.university = target;
       }
-  }
+
+      this.applyFilters();
+    }
+
+    applyFilters(){
+      let tempCourses = this.firebaseService.getDB().list('/courses');
+      tempCourses.forEach(list => {
+        for(let x = 0; x < list.length; x++){
+          if(
+            ( (list[x].title != this.appliedFilters.title) || (this.appliedFilters.title != '') ) &&
+            ( (list[x].courseID != this.appliedFilters.courseID) || (this.appliedFilters.courseID != '') ) &&
+            ( (list[x].professor != this.appliedFilters.professor) || (this.appliedFilters.professor != '') ) &&
+            ( (list[x].university != this.appliedFilters.university) || (this.appliedFilters.university != '') )
+          ){
+            //This is in case I ever need to find courses that do not match the filter.
+          }else{
+            //This is where you handle courses that do match the filter.
+            console.log(list[x])
+            this.coursesToShow.push(list[x]);
+          }
+        }
+      });
+      this.courses = this.coursesToShow;
+    }
+
 }
