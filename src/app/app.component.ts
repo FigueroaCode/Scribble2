0cc85c8168cc9eae84d05abc52eadb32f0c32eb5
debrian <debrianfigueroa@gmail.com>
2017-10-29 22:04:04 -0400

    i dont know anymore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import * as $ from 'jquery';
+import * as $ from 'jquery.transit';
 
 import { HomePage } from '../pages/home/home';
 
